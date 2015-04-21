cd23d2fed7b14b93928fc1aeb60f9a72063a2d1f
Marek <marek.krzysztof.helak@gmail.com>
2015-04-21 19:55:24 +0200

    Initial Commit

---
+++ Tap Master/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Tap Master
+//
+//  Created by Marek on 21.04.2015.
+//  Copyright (c) 2015 Marek Helak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Tap Master/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Tap Master
+//
+//  Created by Marek on 21.04.2015.
+//  Copyright (c) 2015 Marek Helak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
